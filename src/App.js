9c8e06b23f1f5c67e669ea20ae2a96536149dfa8
nicoliprogramer <imotivacionesf@gmail.com>
2022-09-15 01:26:18 -0300

    optimizando

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import ItemDetailContainer from "./componentes/ItemDetailContainer";
 import ItemListContainer from "./componentes/ItemListContainer";
 import NavBar from "./componentes/NavBar";
-import Cart from "./componentes/Cart";
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
@@ -13,7 +12,6 @@ function App() {
         <Routes>
           <Route path='/' element={<ItemListContainer />} />
           <Route path='/category/:categoryId' element={<ItemListContainer />} />
-          <Route path='/cart' element={<Cart />} />
           <Route path='/:category/:id' element={<ItemDetailContainer />} />
         </Routes>
 
